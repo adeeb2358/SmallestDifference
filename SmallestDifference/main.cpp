0a0eb402730601aebf3a877e550d4bf16c956efe
Adeeb Mohammed <adeeb2358@gmail.com>
2020-01-11 23:56:27 +0900

    Initial Commit

---
+++ SmallestDifference/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SmallestDifference
+//
+//  Created by adeeb mohammed on 11/01/20.
+//  Copyright © 2020 adeeb mohammed. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
